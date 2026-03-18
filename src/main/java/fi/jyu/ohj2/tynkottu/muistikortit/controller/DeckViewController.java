3ec3120f96289ddde0a1aa0f737e200552836410
Tuisku Tynkkynen <tuisku.tynkkynen@gmail.com>
2026-03-18 18:46:24 +0200

    Added scene transitions to Deck View

--- src/main/java/fi/jyu/ohj2/tynkottu/muistikortit/controller/DeckViewController.java
+++ src/main/java/fi/jyu/ohj2/tynkottu/muistikortit/controller/DeckViewController.java
@@ -1,13 +1,19 @@
 package fi.jyu.ohj2.tynkottu.muistikortit.controller;
 
+import fi.jyu.ohj2.tynkottu.muistikortit.App;
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
 import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -45,7 +51,14 @@ public class DeckViewController implements Initializable {
     }
 
     private void switchToCardView() {
-        IO.println("switch to card view");
+        try {
+            FXMLLoader loader = new FXMLLoader(App.class.getResource("card_view.fxml"));
+            Stage stage = (Stage) deckCenterButton.getScene().getWindow();
+            stage.setTitle("Flashcards Play");
+            stage.setScene(new Scene(loader.load()));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
@@ -60,7 +73,14 @@ public class DeckViewController implements Initializable {
 
     @FXML
     public void handleDeckEdit(ActionEvent event) {
-        IO.println("Edit Deck");
+        try {
+            FXMLLoader loader = new FXMLLoader(App.class.getResource("edit_view.fxml"));
+            Stage stage = (Stage) deckCenterButton.getScene().getWindow();
+            stage.setTitle("Flashcards Edit");
+            stage.setScene(new Scene(loader.load()));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
