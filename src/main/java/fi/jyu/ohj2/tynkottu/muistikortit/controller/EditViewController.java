09a3a320f06f23ae3cefe03ac40d4d3b2849f9cf
Tuisku Tynkkynen <tuisku.tynkkynen@gmail.com>
2026-03-18 19:12:24 +0200

    Added exit button and scene transition to Edit View

--- src/main/java/fi/jyu/ohj2/tynkottu/muistikortit/controller/EditViewController.java
+++ src/main/java/fi/jyu/ohj2/tynkottu/muistikortit/controller/EditViewController.java
@@ -1,14 +1,18 @@
 package fi.jyu.ohj2.tynkottu.muistikortit.controller;
 
+import fi.jyu.ohj2.tynkottu.muistikortit.App;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,25 +20,20 @@ import java.util.ResourceBundle;
 public class EditViewController implements Initializable {
     @FXML
     private Label cardCountLabel;
-
     @FXML
     private Button cardCreateButton;
-
     @FXML
     private Button cardDeleteButton;
-
+    @FXML
+    private Button exitButton;
     @FXML
     private TextArea cardDescriptionArea;
-
     @FXML
     private TableView<Void> cardTable;
-
     @FXML
     private TextField cardTitleField;
-
     @FXML
     private TextArea deckDescriptionArea;
-
     @FXML
     private TextField deckTitleField;
 
@@ -57,4 +56,20 @@ public class EditViewController implements Initializable {
         IO.println("Delete Card");
     }
 
+    @FXML
+    void handleSave(ActionEvent event) {
+        IO.println("Saved");
+    }
+
+    @FXML
+    void handleExit(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(App.class.getResource("deck_view.fxml"));
+            Stage stage = (Stage) exitButton.getScene().getWindow();
+            stage.setTitle("Flashcards");
+            stage.setScene(new Scene(loader.load()));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
